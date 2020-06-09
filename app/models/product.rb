30d66112870a11a88663d7fcdc02de27bf8c41b1
jamescodegaard <jamescodegaard@gmail.com>
2020-06-09 12:46:47 -0500

    adds image model

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,12 @@ class Product < ApplicationRecord
   validates :price, presence: true, numericality: { greater_than: 0 }
   validates :description, length: { in: 10...500 }
 
+  belongs_to :supplier
+  # def supplier
+  #   Supplier.find_by(id: supplier_id)
+  # end
+  has_many :images
+  
   def is_discounted?
     price < 10
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
-# product = Product.new(name: "Gucci Slides", price: 10, image_url: "https://cache.mrporter.com/variants/images/666467151987779/fr/w2000_q80.jpg", description: "Not counterfeit.")
-# product.save
+Supplier.create!([
+  {name: "Gucci", email: "gucci@gucci.guc", phone_number: "867-5409"},
+  {name: "BahamaMama", email: "bahama@gmail.com", phone_number: "354-3928"}
+])
 
-# new_product = Product.new(name: "Hawaiian Shirt", price: 35, image_url: "https://35f0jq1fcotly82b3o727xi0-wpengine.netdna-ssl.com/wp-content/uploads/rocky-island-palms-hawaiian-shirt-8-510x765.jpg", description: "Definitely your dad's favorite.")
-# new_product.save
+Product.create!([
+  {name: "Hawaiian Shirt", price: "35.0", image_url: "https://35f0jq1fcotly82b3o727xi0-wpengine.netdna-ssl.com/wp-content/uploads/rocky-island-palms-hawaiian-shirt-8-510x765.jpg", description: "Back to the original description... something about this being your dad's favorite.", in_stock: true, supplier_id: 1},
+  {name: "Blue Teapot", price: "45.0", image_url: "https://primulaproducts.com/wp-content/uploads/2013/04/PCI-4340-floral-cast-teapot-3-4-view-2.jpg", description: "A lil short & stout, but I have a handle and a spout!", in_stock: true, supplier_id: 1},
+  {name: "Rainbow Lapel Pin", price: "23.0", image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQgnE2rj870Co3d7nO1dM6Wp6ZZ6Iz-iTeiL4mByD9cze3FgxX33u5ykIBaW8Ef2pekWnubu_Nf&usqp=CAc", description: "Pin it up, dude", in_stock: true, supplier_id: 1},
+  {name: "Cage Pants", price: "28.0", image_url: "https://www.dhresource.com/0x0/f2/albu/g9/M00/E6/99/rBVaWF4S-ACAD0gpAABALJ2Cb3Y708.jpg", description: "Truly a treasure of the nation.", in_stock: true, supplier_id: 1},
+  {name: "Gucci Slides", price: "9.0", image_url: "https://cache.mrporter.com/variants/images/666467151987779/fr/w2000_q80.jpg", description: "Not counterfeit.", in_stock: true, supplier_id: 1}
+])
 
-
-
+Image.create!([
+  {url: "https://cache.mrporter.com/variants/images/666467151987779/fr/w2000_q80.jpg", product_id: 5},
+  {url: "https://primulaproducts.com/wp-content/uploads/2013/04/PCI-4340-floral-cast-teapot-3-4-view-2.jpg", product_id: 2},
+  {url: "https://35f0jq1fcotly82b3o727xi0-wpengine.netdna-ssl.com/wp-content/uploads/rocky-island-palms-hawaiian-shirt-8-510x765.jpg", product_id: 1},
+  {url: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQgnE2rj870Co3d7nO1dM6Wp6ZZ6Iz-iTeiL4mByD9cze3FgxX33u5ykIBaW8Ef2pekWnubu_Nf&usqp=CAc", product_id: 3},
+  {url: "https://www.dhresource.com/0x0/f2/albu/g9/M00/E6/99/rBVaWF4S-ACAD0gpAABALJ2Cb3Y708.jpg", product_id: 4}
+])
